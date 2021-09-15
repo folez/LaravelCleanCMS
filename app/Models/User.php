70799f4e14e87bcfa8d42ad97e8b394fd847f37c
FoLez <folez1992@gmail.com>
2021-09-15 23:20:37 +0300

    Add php dock block from autocomplete support

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+/**
+ * @property string name
+ * @property string email
+ * @property string password
+ * @property int id
+ * */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
