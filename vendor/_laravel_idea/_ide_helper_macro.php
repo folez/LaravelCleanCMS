0aaa31f3cfe925bc640d99b136a567194bd0a467
FoLez <pbh.folez@gmail.com>
2021-10-26 13:31:12 +0300

    Update to laravel 8.6.4

---
+++ vendor/_laravel_idea/_ide_helper_macro.php
@@ -0,0 +1,60 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace Illuminate\Contracts\View {
+    
+    /**
+     * @method $this layoutData($data = [])
+     * @method $this layout($view, $params = [])
+     * @method $this extends($view, $params = [])
+     * @method $this section($section)
+     * @method $this slot($slot)
+     */
+    class View {}
+}
+
+namespace Illuminate\Http {
+    
+    /**
+     * @method array validate(array $rules, ...$params)
+     * @method array validateWithBag(string $errorBag, array $rules, ...$params)
+     * @method bool hasValidSignature($absolute = true)
+     * @method bool hasValidRelativeSignature()
+     */
+    class Request {}
+}
+
+namespace Illuminate\Testing {
+    
+    /**
+     * @method $this assertSeeLivewire($component)
+     * @method $this assertDontSeeLivewire($component)
+     */
+    class TestResponse {}
+    
+    /**
+     * @method $this assertSeeLivewire($component)
+     * @method $this assertDontSeeLivewire($component)
+     */
+    class TestView {}
+}
+
+namespace Illuminate\View {
+
+    use Livewire\WireDirective;
+    
+    /**
+     * @method WireDirective wire($name)
+     */
+    class ComponentAttributeBag {}
+    
+    /**
+     * @method $this layoutData($data = [])
+     * @method $this layout($view, $params = [])
+     * @method $this extends($view, $params = [])
+     * @method $this section($section)
+     * @method $this slot($slot)
+     */
+    class View {}
+}
---
+++ vendor/_laravel_idea/_ide_helper_macro_static.php
@@ -0,0 +1,60 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace Illuminate\Contracts\View {
+    
+    /**
+     * @method static $this layoutData($data = [])
+     * @method static $this layout($view, $params = [])
+     * @method static $this extends($view, $params = [])
+     * @method static $this section($section)
+     * @method static $this slot($slot)
+     */
+    class View {}
+}
+
+namespace Illuminate\Http {
+    
+    /**
+     * @method static array validate(array $rules, ...$params)
+     * @method static array validateWithBag(string $errorBag, array $rules, ...$params)
+     * @method static bool hasValidSignature($absolute = true)
+     * @method static bool hasValidRelativeSignature()
+     */
+    class Request {}
+}
+
+namespace Illuminate\Testing {
+    
+    /**
+     * @method static $this assertSeeLivewire($component)
+     * @method static $this assertDontSeeLivewire($component)
+     */
+    class TestResponse {}
+    
+    /**
+     * @method static $this assertSeeLivewire($component)
+     * @method static $this assertDontSeeLivewire($component)
+     */
+    class TestView {}
+}
+
+namespace Illuminate\View {
+
+    use Livewire\WireDirective;
+    
+    /**
+     * @method static WireDirective wire($name)
+     */
+    class ComponentAttributeBag {}
+    
+    /**
+     * @method static $this layoutData($data = [])
+     * @method static $this layout($view, $params = [])
+     * @method static $this extends($view, $params = [])
+     * @method static $this section($section)
+     * @method static $this slot($slot)
+     */
+    class View {}
+}
