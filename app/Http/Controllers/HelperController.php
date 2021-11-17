aadee7c69e00017a24495e3f0f843c09392aa2a1
FoLez <pbh.folez@gmail.com>
2021-11-17 16:32:15 +0200

    Change language function review

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -2,25 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Language;
+use Illuminate\Http\Request;
+
 class HelperController extends Controller
 {
-    public function changeLang(\Illuminate\Http\Request $request, $lng)
+    public function changeLang(Request $request, $lang)
     {
-        $langs = \App\Models\Language::all()->map( fn($x) => $x->code )->toArray();
-        $l = array_search($lng, $langs);
-        unset($langs[$l]);
-        $referrer = str_replace(asset(''),'',$request->header('referer'));
-        $referrer = explode('/', $referrer);
-        //    dd($referrer);
-        $t = array_needle_array_search($langs, $referrer);
-        if(count($referrer) > 1) {
-            unset($referrer[$t]);
-        } else {
-            array_unshift($referrer, $lng);
+        $host = url('/');
+
+        $url = $request->header('referer');
+
+        if ( strpos( $url, $host ) === false ) {
+            return $url;
         }
-        //        $referrer = end( $referrer);
-        \App::setLocale($lng);
-        \Session::put('locale', $lng);
-        return redirect()->to(implode('/',$referrer));
+
+        $default_uri = str_replace( $host, '', $url );
+
+        $default_uri = $default_uri ? $default_uri : '/';
+        $languages   = Language::all();
+
+        $parts       = explode( '/', ltrim( $this->trailingslashit( $default_uri ), '/' ) );
+
+        $url_lang    = $parts[0];
+        if ( $languages->contains('code', $url_lang) ) {
+            $default_uri = preg_replace( '!^/' . $url_lang . '(/|$)!i', '/', $default_uri );
+        }
+
+        $defaultLanguageModel = Language::getDefaultLanguage();
+
+        $default_language = $defaultLanguageModel->code;
+
+        if ( $lang === $default_language ) {
+            $new_uri = '/' . $default_uri;
+        } else {
+            $new_uri = '/' . $lang . $default_uri;
+        }
+
+        $new_uri = preg_replace( '/(\/+)/', '/', $new_uri );
+
+        if ( '/' !== $new_uri ) {
+            $new_url = $host . $new_uri;
+        } else {
+            $new_url = $host;
+        }
+        //        dd(redirect()->to($new_url));
+        \App::setLocale($lang);
+        \Session::put('locale', $lang);
+        return redirect()->to($new_url);
+    }
+
+    private function untrailingslashit($string)
+    {
+        return rtrim( $string, '/\\' );
+    }
+
+    private function trailingslashit( $string ) {
+        return $this->untrailingslashit( $string ) . '/';
     }
 }
