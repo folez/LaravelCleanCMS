79271a0b54d236c78ceb4e24d696a0d7d124e5ec
FoLez <pbh.folez@gmail.com>
2021-11-18 09:39:10 +0200

    Update Translatable.php trait

--- app/Traits/Translatable.php
+++ app/Traits/Translatable.php
@@ -16,26 +16,46 @@ trait Translatable
 {
     /**
      * @param Builder $builder
-     * @param string  $code
+     * @param integer $id
+     * @param string  $columnName
+     * @return Builder
+     * @static
+     */
+    public function scopeLanguageCodeAllAndId(Builder $builder, int $id , string $columnName = 'name') : Builder
+    {
+        return  $builder->select("{$this->table}.*","translation.*")
+            ->where($this->table.'.id', '=', $id)->whereNotNull('translation.'.$columnName)
+            ->join($this->translationTableName.' as translation', $this->table.'.id', '=', 'translation'.'.'.$this->translationFkName)
+            ->groupBy("translation.{$this->translationFkName}");
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $columnName @default name
+     * @return Builder
+     * @static
+     */
+    public function scopeLanguageCodeAll(Builder $builder, string $columnName = 'name') : Builder
+    {
+        $code = Language::select('id')->get();
+        return  $builder->
+        select("{$this->table}.*","translation.*")->whereNotNull('translation.'.$columnName)->
+        join($this->translationTableName.' as translation',
+            $this->table.'.id',
+            '=',
+            'translation'.'.'.$this->translationFkName)
+            ->groupBy("translation.{$this->translationFkName}");
+
+    }
+
+    /**
+     * @param Builder $builder
      * @return Builder
      * @static
      */
     public function scopeLanguageCode( Builder $builder ) : Builder
     {
-        $code = app()->getLocale();
-        foreach (Language::where('is_default', '=',false)->get() as $lang){
-            if(\Request::segment(1) == "{$lang->code}"){
-                if(session()->get('locale')){
-                    $locale = session()->get('locale') ?? $lang->code;
-                    app()->setLocale($locale);
-                    $code = $locale;
-                } else {
-                    app()->setLocale($lang->code);
-                    $code = $lang->code;
-                }
-                //                app()->setLocale($lang->code);
-            }
-        }
+        $code = $this->getLanguageCode();
 
         return $builder
             ->join($this->translationTableName.' as translation',
@@ -44,30 +64,20 @@ trait Translatable
                 'translation'.'.'.$this->translationFkName)
             ->where('translation'.'.language_id','=',function (\Illuminate\Database\Query\Builder $builder) use ($code){
                 $builder->from(Language::TABLE_NAME)->select('id')->where('code','=',$code);
-            });
+            })->whereNotNull('translation.name');
     }
+
+
+
     /**
      * @param Builder $builder
-     * @param int $id
-     * @return Builder
+     * @param int $modelId
+     * @return Model
      * @static
      */
     public function scopeFindByLanguageCodeAndId( Builder $builder, int $modelId ) : Model
     {
-        $code = app()->getLocale();
-        foreach (Language::where('is_default', '=',false)->get() as $lang){
-            if(\Request::segment(1) == "{$lang->code}"){
-                if(session()->get('locale')){
-                    $locale = session()->get('locale') ?? $lang->code;
-                    app()->setLocale($locale);
-                    $code = $locale;
-                } else {
-                    app()->setLocale($lang->code);
-                    $code = $lang->code;
-                }
-                //                app()->setLocale($lang->code);
-            }
-        }
+        $code = $this->getLanguageCode();
 
         return $builder
             ->where($this->table.'.id','=',$modelId)
@@ -78,5 +88,50 @@ trait Translatable
             ->where('translation'.'.language_id','=',function (\Illuminate\Database\Query\Builder $builder) use ($code){
                 $builder->from(Language::TABLE_NAME)->select('id')->where('code','=',$code);
             })->first();
+
+        //        ->whereNotNull('translation.name')
+    }
+
+    /**
+     * @param Builder $builder
+     * @param int $id
+     * @return Builder
+     * @static
+     */
+    public function scopeFindByLanguageCodeAndSlug( Builder $builder, string $slug ) : Model
+    {
+        $code = $this->getLanguageCode();
+
+        return $builder
+            ->where('slug', '=', $slug )
+            ->join($this->translationTableName.' as translation',
+                $this->table.'.id',
+                '=',
+                'translation'.'.'.$this->translationFkName)
+            ->where('translation'.'.language_id','=',function (\Illuminate\Database\Query\Builder $builder) use ($code){
+                $builder->from(Language::TABLE_NAME)->select('id')->where('code','=',$code);
+            })->first();
+
+        //        ->whereNotNull('translation.name')
+    }
+
+    private function getLanguageCode()
+    {
+        $code = app()->getLocale();
+
+        foreach (Language::where('is_default', '=',false)->get() as $lang){
+            if(\Request::segment(1) == "{$lang->code}"){
+                if(session()->get('locale')){
+                    $locale = session()->get('locale') ?? $lang->code;
+                    app()->setLocale($locale);
+                    $code = $locale;
+                } else {
+                    app()->setLocale($lang->code);
+                    $code = $lang->code;
+                }
+                //                app()->setLocale($lang->code);
+            }
+        }
+        return $code;
     }
 }
