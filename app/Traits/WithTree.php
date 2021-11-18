fd1dbba0ae266610b791dd69e254d31182903aba
FoLez <pbh.folez@gmail.com>
2021-11-18 10:02:27 +0200

    Create WithTree trait for model with tree view component

---
+++ app/Traits/WithTree.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+trait WithTree
+{
+    /**
+     * @var string
+     */
+    protected string $childModel;
+    /**
+     * @var string
+     */
+    protected string $childForeignId;
+
+    /**
+     * @var array
+     */
+    protected array $routeNames;
+
+    /**
+     * @var bool
+     */
+    protected bool $showEditLink = true;
+    /**
+     * @var bool
+     */
+    protected bool $showCreateLink = true;
+
+    /**
+     * @return int
+     */
+    public function hasChildren(): int
+    {
+        /** @var HasRelationships $this  */
+        return $this->hasMany($this->childModel, $this->childForeignId, $this->primaryKey)->count();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEditLink(): ?string
+    {
+        return $this->showEditLink ? route('admin.services.edit', ['id' => $this->id]) : null;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCreateLink(): ?string
+    {
+        return $this->showCreateLink ? route('admin.services.category.create').'?selectedCategory='.$this->id : null;
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function items(): HasMany
+    {
+        /** @var HasRelationships $this  */
+        return $this->hasMany($this->childModel, $this->childForeignId, $this->primaryKey);
+    }
+}
